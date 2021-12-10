4d8e519820eae533874d42851da463865d1204db
Yash Sahu <itsyashsahu@gmail.com>
2021-12-10 12:33:53 +0530

    make it better

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use( "/transhistory"   , require( './routes/transHistory') )
 
 if(process.env.NODE_ENV=="production"){
     app.use(express.static('client/build'))
-    const path = require('path')
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
