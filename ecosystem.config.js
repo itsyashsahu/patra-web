ca63563cd029b35d23ba7ebf89c07ebc5381640f
itsyashsahu <itsyashsahu@gmail.com>
2022-05-26 18:04:19 +0530

    made some changes

---
+++ ecosystem.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  apps: [
+    {
+      name: "Unsung",
+      script: "app.js",
+      instance: 0,
+      autorestart: true,
+      exec_mode: "cluster",
+      watch: true,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
+
+// module.exports = {
+//   apps: [
+//     {
+//       name: "app",
+//       script: "./server.js",
+//       env: {
+//         NODE_ENV: "development",
+//       },
+//       env_production: {
+//         NODE_ENV: "production",
+//       },
+//     },
+//     {
+//       name: "worker",
+//       script: "worker.js",
+//     },
+//   ],
+// };
