f5b1d353c681cd8b90548a16b01e51547f92aacb
Yash Sahu <itsyashsahu@gmail.com>
2023-01-25 10:37:02 +0530

    resolving the dependency issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 var cors = require("cors");
 const path = require("path");
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
